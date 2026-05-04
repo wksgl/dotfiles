7aa5353f32972b7e52bc1847cc698ae01a51bcc2
wksgl <wksgl@gmail.com>
2026-05-04 20:34:50 +0800

    nvim: nvim-tree -> neo-tree (支持侧边栏 / 搜索文件)

--- home/.config/nvim/lua/plugins/tree.lua
+++ home/.config/nvim/lua/plugins/tree.lua
@@ -1,42 +1,45 @@
 return {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
-    lazy = false,
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
     dependencies = {
+        "nvim-lua/plenary.nvim",
         "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
     },
-    config = function ()
-        local api = require("nvim-tree.api")
+    config = function()
+        vim.keymap.set("n", "<Leader><Tab>", ":Neotree toggle<CR>", { desc = "Toggle file tree", silent = true })
 
-        require("nvim-tree").setup {
-            view = {
-                width = 40,
-                side = "left",
+        -- / 在树中搜索过滤文件名
+        -- H 返回根目录
+        -- <BS> 返回上级目录
+        -- <CR> 打开文件
+        -- a 新建文件/目录
+
+        require("neo-tree").setup({
+            close_if_last_window = true,
+            popup_border_style = "rounded",
+            window = {
+                width = 30,
+                mappings = {
+                    ["/"] = "filter_on_submit", -- 搜索过滤文件
+                    ["H"] = "navigate_up",
+                    ["l"] = "open",
+                    ["h"] = "close_node",
+                    ["<bs>"] = "navigate_up",
+                },
             },
-            renderer = {
-                icons = {
-                    glyphs = {
-                        folder = {
-                            arrow_closed = "▶",
-                            arrow_open = "▼",
-                        },
+            filesystem = {
+                filtered_items = {
+                    hide_dotfiles = false,
+                    hide_gitignored = false,
+                },
+                window = {
+                    mappings = {
+                        ["/"] = "filter_on_submit",
+                        ["<bs>"] = "navigate_up",
                     },
                 },
             },
-            actions = {
-                open_file = {
-                    quit_on_open = true,  -- 打开文件后退出树窗口
-                    window_picker = {
-                        enable = true,
-                    },
-                },
-            },
-            filters = {
-                dotfiles = false,  -- 显示点文件
-            },
-        }
-
-        -- 全局映射：Leader+Tab 切换 nvim-tree
-        vim.keymap.set("n", "<Leader><Tab>", api.tree.toggle, { desc = "Toggle nvim-tree", noremap = true, silent = true })
-    end
+        })
+    end,
 }
